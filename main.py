6e5cd9a966271ad61595d8be310f2002d4dc0a0c
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-09-11 20:13:27 +0530

    api fix

--- main.py
+++ main.py
@@ -8,17 +8,21 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 app = FastAPI()
-
+arr = []
 class Item(BaseModel):
     name:str
     price:int
+
+
 
 @app.get("/")
 def home():
     return {"msg":"hellow"}
 
-@app.put("/item/{itemId}")
+@app.put("/itemput/{itemId}")
 def updatitem(itemId:int , item:Item):
+    temp = {"Id":itemId,"name":item.name,"price":item.price}
+    arr.append(temp)
     return {"item name":item.name,"itemId":itemId,"price":item.price}
 
 @app.post("/item/{name}") 
@@ -27,10 +31,14 @@ def updatePrice(name:str,item:Item):
     return {"itemId":name,"Price":item.price}
 
 @app.get("/item/{itemId}")
-def getItem(itemId:int,name:str,price:int):
-    if(price == 0):
-        return {"msg":"No price sent"}
-    return {"itemId":itemId,"name":name,"price":price}
+def getItem(itemId:int):
+    res = {}
+    for x in arr:
+        if  x["Id"] == itemId:
+            print("type:",x["Id"])
+            res = x
+            return x
+    return {"msg":"item not found"}
 
 
 def generate_token(data:dict):
